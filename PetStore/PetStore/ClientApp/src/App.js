3e751b4342a77d8aa13026cdbf096988c717eff4
SimonaMDimitrova <simona.m.dimitrova@gmail.com>
2021-04-03 09:17:04 +0300

    Remove favicon

--- PetStore/PetStore/ClientApp/src/App.js
+++ PetStore/PetStore/ClientApp/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
-import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
-import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
-import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
 
 import './custom.css'
 
